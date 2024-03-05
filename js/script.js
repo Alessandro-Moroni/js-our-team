ea96e18b8d1ce9b04ba4a834dfebe77b68d70717
Alessandro-Moroni <alemoro98@gmail.com>
2024-03-05 15:10:57 +0100

    [start]repo

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const people = [
+  {
+    img: 'img/angela-caroll-chief-editor.jpg',
+    name: 'Wayne Barnett',
+    work: 'Founder & CEO'
+  },
+  {
+    img: 'img/angela-caroll-chief-editor.jpg',
+    name: 'Angelica Caroll',
+    work: 'Chief Editor'
+  },
+  {
+    img: 'img/angela-caroll-chief-editor.jpg',
+    name: 'Walter Gordon',
+    work: 'Office Manager'
+  },
+  {
+    img: 'img/angela-caroll-chief-editor.jpg',
+    name: 'Angela Lopez',
+    work: 'Social Media Manager'
+  },
+  {
+    img: 'img/angela-caroll-chief-editor.jpg',
+    name: 'Scott Estrada',
+    work: 'Developer'
+  },
+  {
+    img: 'img/angela-caroll-chief-editor.jpg',
+    name: 'Barbara Ramos',
+    work: 'Graphic Designer'
+  }
+]
