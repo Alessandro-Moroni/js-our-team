82af1d636eee372f85a31da4de83071da327d5bb
Alessandro-Moroni <alemoro98@gmail.com>
2024-03-05 15:29:26 +0100

    [finish]

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const output = document.querySelector('.output')
 
 const people = [
   {
-    img: 'img/angela-caroll-chief-editor.jpg',
+    img: 'img/wayne-barnett-founder-ceo.jpg',
     name: 'Wayne Barnett',
     work: 'Founder & CEO'
   },
@@ -12,22 +12,22 @@ const people = [
     work: 'Chief Editor'
   },
   {
-    img: 'img/angela-caroll-chief-editor.jpg',
+    img: 'img/walter-gordon-office-manager.jpg',
     name: 'Walter Gordon',
     work: 'Office Manager'
   },
   {
-    img: 'img/angela-caroll-chief-editor.jpg',
+    img: 'img/angela-lopez-social-media-manager.jpg',
     name: 'Angela Lopez',
     work: 'Social Media Manager'
   },
   {
-    img: 'img/angela-caroll-chief-editor.jpg',
+    img: 'img/scott-estrada-developer.jpg',
     name: 'Scott Estrada',
     work: 'Developer'
   },
   {
-    img: 'img/angela-caroll-chief-editor.jpg',
+    img: 'img/barbara-ramos-graphic-designer.jpg',
     name: 'Barbara Ramos',
     work: 'Graphic Designer'
   }
@@ -35,4 +35,17 @@ const people = [
 
 for(let person of people){
   console.log(person);
+  output.innerHTML += `
+
+  <div class="col mb-3 ">
+          <div class="card" style="width: 18rem;">
+            <img src="${person.img}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-text">${person.name}</h5>
+              <p class="card-text">${person.work}</p>
+            </div>
+          </div>
+        </div>
+
+  `
 }
